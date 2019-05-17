0089fedb210118911cab6f91373a5838212cffc0
AFranco92 <afranco.t92@gmail.com>
2019-05-16 22:17:49 -0300

    module_1 - 1 - index.html with section fading in after the page load

---
+++ module_1/assets/js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded",function() {
+	let greetssection = document.getElementsByClassName("content")[0];
+	greetssection.classList.replace("hide", "show");
+});
