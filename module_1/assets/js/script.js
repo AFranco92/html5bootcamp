4e59df59258ccfd38fabdbbd122549f6d08c8974
AFranco92 <afranco.t92@gmail.com>
2019-05-16 22:36:15 -0300

    module_1 - 2 - Added a button below the section that calls a function when clicked. That function showcases an alert message

--- module_1/assets/js/script.js
+++ module_1/assets/js/script.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded",function() {
+
 	let greetssection = document.getElementsByClassName("content")[0];
 	greetssection.classList.replace("hide", "show");
+
+	let showMessage = () => {alert("You pressed me!");}
+
+	let callbutton = document.getElementsByClassName("button")[0];
+	callbutton.addEventListener("click", showMessage);
+
 });
