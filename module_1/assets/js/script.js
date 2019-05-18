7fdc75a1d9fc419da937d57cb97e9b930a58d9bd
AFranco92 <afranco.t92@gmail.com>
2019-05-18 01:37:23 -0300

    module_1 - 3 - Data fetching. When the button is clicked, a new joke is shown in the section element

--- module_1/assets/js/script.js
+++ module_1/assets/js/script.js
@@ -3,9 +3,30 @@ document.addEventListener("DOMContentLoaded",function() {
 	let greetssection = document.getElementsByClassName("content")[0];
 	greetssection.classList.replace("hide", "show");
 
-	let showMessage = () => {alert("You pressed me!");}
+	//Arrow function that showcases an alert message when called.
+	let showMessage = () => alert("You pressed me!");
 
+	//An object created with its URL.
+	let configobj = {
+		"url" : "http://api.icndb.com/jokes/random"
+	};
+
+	//Reusable ajax method that takes the URL of the object created above.
+	let getAJAXCall = async function() {
+		let url = configobj.url;
+
+		//Could not find another way to show the spinner while loading the content (to avoid mixing of HTML and JS)
+		greetssection.innerHTML = "<img class='spinner' src='assets/images/spinner.gif'></img>";
+		const response = await fetch(url);
+		const json = await response.json();
+		greetssection.innerHTML = json.value.joke; //To fix -> This line of code is not reusable.
+	}
+	getAJAXCall()
+	.catch(error => {
+		console.error(error);
+		greetssection.classList.add("error"); //If an error occurs the error class is added to change the background-color to red.
+	});
+	
 	let callbutton = document.getElementsByClassName("button")[0];
-	callbutton.addEventListener("click", showMessage);
-
+	callbutton.addEventListener("click", getAJAXCall);
 });
