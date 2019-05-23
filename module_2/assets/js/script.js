dde087f2ca1e860a99f7f694da1a120273d392a7
AFranco92 <afranco.t92@gmail.com>
2019-05-23 01:10:42 -0300

    module_2 - 3 - Working with classes. Added a method that allows the addition of multiple actors to a movie; created the class Logger that listen to Movie's events

--- module_2/assets/js/script.js
+++ module_2/assets/js/script.js
@@ -8,13 +8,13 @@ document.addEventListener("DOMContentLoaded",function() {
 
 	class EventEmitter {
 		listeners = {};
+		observers = [];
 		addListener(eventName, callback) {
 			//if the event exists, returns the array. if not, an empty one.
 			this.listeners[eventName] = this.listeners[eventName] || [];
 			this.listeners[eventName].push(callback);
 			return this;
 		}
-
 		removeListener(eventName, callback) {
 			//getting the callbacks for that event.
 		    let callbacks = this.listeners[eventName];
@@ -48,24 +48,64 @@ document.addEventListener("DOMContentLoaded",function() {
 		    	//else, each callback is called.
 		    	callbacks.forEach((cb) => { 
 		    	cb();
+		    	this.notify();
 		    });
 		    return true;
 		}
 		off(eventName, callback) {
 			return this.removeListener(eventName, callback);
 		}
+		addObserver(observer) {
+    		this.observers.push(observer);
+  		}
+  		removeObserver(observer) {
+  			//if the observers array is empty, returns false.
+			if(!this.observers) { return false; }
+
+			//else, for each observer asks if the name equals to the parameter observer's name.
+			for(let i = 0; i < this.observers.length; i++) {
+				if(this.observers[i].name === observer.name) {
+
+					//if equals, the observer is deleted from the array.
+					this.observers.splice(observer, 1);
+					return true;
+				}
+			}
+  		}
+		notify(eventName) {
+
+			//if the array of observers is not empty, it call the log function of them.
+			if(this.observers.length > 0) {
+				this.observers.forEach(observer => observer.log(eventName));
+			}
+		}
 	}
 
 	class Movie extends EventEmitter {
-		constructor(name, year, duration, listeners) {
+		constructor(name, year, duration, actors, listeners) {
 			super(listeners);
 			this.name = name;
 			this.year = year;
 			this.duration = duration;
+			this.actors = [];
 		}
-		play = () => alert("The movie "+this.name+" has started");
-		pause = () => alert("The movie "+this.name+" has stopped");
-		resume = () => alert("The movie "+this.name+" has restarted");
+
+		//every function notifies to the observers after its update.
+		play() {
+			alert("The movie "+this.name+" has started");
+			this.notify("play");
+		}
+		pause() {
+			alert("The movie "+this.name+" has stopped");
+			this.notify("pause");
+		}
+		resume() {
+			alert("The movie "+this.name+" has restarted");
+			this.notify("resume");
+		}
+		addCast(cast) {
+			this.actors.push(cast);
+		}
 	}
 
 	class Actor {
@@ -75,14 +115,30 @@ document.addEventListener("DOMContentLoaded",function() {
 		}
 	}
 
+	class Logger {
+		constructor(name) {
+			this.name = name;
+		}
+		log(eventName) {
+			alert("The "+eventName+" event has been emitted");
+		}
+	}
+
+	let logger1 = new Logger("logger1");
 	let matilda = new Movie("Matilda", "1996", "1h42m");
 	let titanic = new Movie("Titanic", "1997", "3h15m");
 	let ironman = new Movie("Iron-Man", "2008", "2h6m");
 	let eventemitter1 = new EventEmitter();
+	matilda.addObserver(logger1);
+	//matilda.removeObserver(logger1);
+	console.log(matilda.observers);
+
+	//adding two actors to the movie.
+	matilda.addCast([new Actor("Danny DeVito", 74), new Actor("Mara Wilson", 31)]);
 
 	eventemitter1.on("click", displayAlert);
 	eventemitter1.on("click", displayUpperCaseAlert);
-	eventemitter1.emit("click");
+	//eventemitter1.emit("click");
 
 	//calling inherited function on, using play as the callback.
 	document.querySelector("div").addEventListener("click", () => matilda.on("click", matilda.play()));
