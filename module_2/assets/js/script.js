64fb6337bbbc7385dee4ad6be87c18b58be66122
AFranco92 <afranco.t92@gmail.com>
2019-05-23 10:18:22 -0300

    module_2 - 4 - Mixins. Added the object social with methods share and like and then, copied that functionality to Movie class

--- module_2/assets/js/script.js
+++ module_2/assets/js/script.js
@@ -124,14 +124,26 @@ document.addEventListener("DOMContentLoaded",function() {
 		}
 	}
 
+	let social = {
+		share(friendName) { 
+			alert(friendName+" share "+this.name); 
+		},
+		like(friendName) { 
+			alert(friendName+" like "+this.name); 
+		}
+	};
+
+	//copying the methods of social to Movie class.
+	Object.assign(Movie.prototype, social);
+
 	let logger1 = new Logger("logger1");
 	let matilda = new Movie("Matilda", "1996", "1h42m");
 	let titanic = new Movie("Titanic", "1997", "3h15m");
 	let ironman = new Movie("Iron-Man", "2008", "2h6m");
+	matilda.like("Franco");
 	let eventemitter1 = new EventEmitter();
 	matilda.addObserver(logger1);
 	//matilda.removeObserver(logger1);
-	console.log(matilda.observers);
 
 	//adding two actors to the movie.
 	matilda.addCast([new Actor("Danny DeVito", 74), new Actor("Mara Wilson", 31)]);
